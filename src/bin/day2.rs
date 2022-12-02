cfc595c9c25b5b64c180dfac6084098b25703549
jsgroth <1137683+jsgroth@users.noreply.github.com>
2022-12-01 23:56:02 -0600

    day 2 solution

---
+++ src/bin/day2.rs
@@ -0,0 +1,96 @@
+#[derive(PartialEq, Eq)]
+enum Play {
+    Rock,
+    Paper,
+    Scissors,
+}
+
+impl Play {
+    fn from_char(c: char) -> Play {
+        match c {
+            'A' | 'X' => Self::Rock,
+            'B' | 'Y' => Self::Paper,
+            'C' | 'Z' => Self::Scissors,
+            _ => panic!("{c} should be A-C or X-Z"),
+        }
+    }
+
+    fn point_value(&self) -> i32 {
+        match self {
+            Self::Rock => 1,
+            Self::Paper => 2,
+            Self::Scissors => 3,
+        }
+    }
+
+    fn get_winning_play(&self) -> Play {
+        match self {
+            Self::Rock => Self::Paper,
+            Self::Paper => Self::Scissors,
+            Self::Scissors => Self::Rock,
+        }
+    }
+
+    fn get_losing_play(&self) -> Play {
+        match self {
+            Self::Rock => Self::Scissors,
+            Self::Paper => Self::Rock,
+            Self::Scissors => Self::Paper,
+        }
+    }
+
+    fn beats(&self, other: &Play) -> bool {
+        *other == self.get_losing_play()
+    }
+}
+
+const WIN_SCORE: i32 = 6;
+const DRAW_SCORE: i32 = 3;
+const LOSE_SCORE: i32 = 0;
+
+fn solve(input: &[String]) -> i32 {
+    let plays: Vec<(Play, Play)> = input.iter().map(|line| {
+        let opponent_play = Play::from_char(line.chars().next().expect("no line should be empty"));
+        let your_play = Play::from_char(line.chars().last().expect("no line should be empty"));
+        (opponent_play, your_play)
+    })
+        .collect();
+
+    plays.iter().map(|(opponent_play, your_play)| {
+        let game_score = if opponent_play == your_play {
+            DRAW_SCORE
+        } else if your_play.beats(opponent_play) {
+            WIN_SCORE
+        } else {
+            LOSE_SCORE
+        };
+
+        game_score + your_play.point_value()
+    })
+        .sum()
+}
+
+fn solve_part_2(input: &[String]) -> i32 {
+    input.iter().map(|line| {
+        let opponent_play = Play::from_char(line.chars().next().expect("no line should be empty"));
+
+        let last_char = line.chars().last().expect("no line should be empty");
+        match last_char {
+            'X' => LOSE_SCORE + opponent_play.get_losing_play().point_value(),
+            'Y' => DRAW_SCORE + opponent_play.point_value(),
+            'Z' => WIN_SCORE + opponent_play.get_winning_play().point_value(),
+            _ => panic!("every line should end in X/Y/Z"),
+        }
+    })
+        .sum()
+}
+
+fn main() {
+    let input = advent_of_code_2022::read_input().expect("unable to read input file");
+
+    let solution1 = solve(&input);
+    println!("{solution1}");
+
+    let solution2 = solve_part_2(&input);
+    println!("{solution2}");
+}
