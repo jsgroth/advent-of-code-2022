4b83b4a5c8ba8dee5724611bc631eaf0113c17d5
jsgroth <1137683+jsgroth@users.noreply.github.com>
2022-12-20 02:21:56 -0600

    use a vec instead of a hand-rolled doubly-linked list, much simpler

--- src/bin/day20.rs
+++ src/bin/day20.rs
@@ -1,193 +1,80 @@
 //! Day 20: Grove Positioning System
 //! https://adventofcode.com/2022/day/20
 
-#[derive(Debug)]
 struct Node {
-    prev: Option<usize>,
-    next: Option<usize>,
     value: i64,
+    original_index: usize,
 }
 
 struct NumberList {
     nodes: Vec<Node>,
     indices: Vec<usize>,
-    head: usize,
-    tail: usize,
 }
 
 impl NumberList {
     fn from_vec(v: Vec<i64>) -> Self {
         let mut nodes = Vec::with_capacity(v.len());
         let mut indices = Vec::with_capacity(v.len());
-        for i in 0..v.len() {
-            let prev = if i == 0 { None } else { Some(i - 1) };
-            let next = if i == v.len() - 1 { None } else { Some(i + 1) };
-            let node = Node {
-                prev,
-                next,
-                value: v[i],
-            };
-
-            nodes.push(node);
+        for (i, n) in v.into_iter().enumerate() {
+            nodes.push(Node { value: n, original_index: i });
             indices.push(i);
         }
-        Self {
-            nodes,
-            indices,
-            head: 0,
-            tail: v.len() - 1,
-        }
+        Self { nodes, indices }
     }
 
-    fn move_num(&mut self, index: usize) {
+    fn move_num(&mut self, i: usize) {
+        let mut index = self.indices[i];
         let n = self.nodes[index].value;
 
-        if n > 0 {
-            for _ in 0..(n % (self.nodes.len() - 1) as i64) {
-                match self.nodes[index].next {
-                    Some(next) => {
-                        self.nodes[index].next = self.nodes[next].next;
-                        self.nodes[next].prev = self.nodes[index].prev;
-
-                        if let Some(prev) = self.nodes[index].prev {
-                            self.nodes[prev].next = Some(next);
-                        }
-                        if let Some(next_next) = self.nodes[next].next {
-                            self.nodes[next_next].prev = Some(index);
-                        }
-
-                        self.nodes[index].prev = Some(next);
-                        self.nodes[next].next = Some(index);
-
-                        if self.nodes[index].next == None {
-                            self.tail = index;
-                        } else if self.nodes[next].prev == None {
-                            self.head = next;
-                        }
-                    }
-                    None => {
-                        let prev = self.nodes[index].prev.unwrap();
-                        self.nodes[prev].next = None;
-                        self.tail = prev;
-
-                        let after_head = self.nodes[self.head].next.unwrap();
-                        self.nodes[index].next = Some(after_head);
-                        self.nodes[after_head].prev = Some(index);
-                        self.nodes[index].prev = Some(self.head);
-                        self.nodes[self.head].next = Some(index);
-                    }
-                }
-            }
-        } else if n < 0 {
-            for _ in 0..(n.abs() % (self.nodes.len() - 1) as i64) {
-                match self.nodes[index].prev {
-                    Some(prev) => {
-                        self.nodes[index].prev = self.nodes[prev].prev;
-                        self.nodes[prev].next = self.nodes[index].next;
-
-                        if let Some(next) = self.nodes[index].next {
-                            self.nodes[next].prev = Some(prev);
-                        }
-                        if let Some(prev_prev) = self.nodes[prev].prev {
-                            self.nodes[prev_prev].next = Some(index);
-                        }
-
-                        self.nodes[index].next = Some(prev);
-                        self.nodes[prev].prev = Some(index);
-
-                        if self.nodes[index].prev == None {
-                            self.head = prev;
-                            self.nodes[prev].prev = None;
-
-                            self.nodes[self.tail].next = Some(index);
-                            self.nodes[index].prev = Some(self.tail);
-                            self.nodes[index].next = None;
-                            self.tail = index;
-                        } else if self.nodes[prev].next == None {
-                            self.tail = prev;
-                        }
-                    }
-                    None => {
-                        let next = self.nodes[index].next.unwrap();
-                        self.nodes[next].prev = None;
-                        self.head = next;
-
-                        let before_tail = self.nodes[self.tail].prev.unwrap();
-                        self.nodes[index].prev = Some(before_tail);
-                        self.nodes[before_tail].next = Some(index);
-                        self.nodes[index].next = Some(self.tail);
-                        self.nodes[self.tail].prev = Some(index);
-                    }
-                }
+        let num_moves = n.abs() % (self.nodes.len() - 1) as i64;
+        for _ in 0..num_moves {
+            if index == 0 && n < 0 {
+                self.swap(0, self.nodes.len() - 1);
+                index = self.nodes.len() - 1;
+            } else if index == self.nodes.len() - 1 && n > 0 {
+                self.swap(0, self.nodes.len() - 1);
+                index = 0;
+            } else {
+                let new_index = (index as i64 + n.signum()) as usize;
+                self.swap(index, new_index);
+                index = new_index;
             }
         }
     }
 
     fn get(&self, i: usize) -> i64 {
-        let mut zero_node = &self.nodes[self.head];
-        while zero_node.value != 0 {
-            zero_node = &self.nodes[zero_node.next.unwrap()];
-        }
-
-        let i = i % self.nodes.len();
-        let mut node = zero_node;
-        for _ in 0..i {
-            match node.next {
-                Some(next) => {
-                    node = &self.nodes[next];
-                }
-                None => {
-                    node = &self.nodes[self.head];
-                }
-            }
-        }
-        node.value
+        let zero_index = self.nodes.iter().position(|node| node.value == 0).expect("list should contain 0");
+        let target_index = (zero_index + i) % self.nodes.len();
+        self.nodes[target_index].value
     }
 
-    fn _print(&self) {
-        // for node in &self.nodes {
-        //     println!("{node:?}");
-        // }
-        // println!("{}, {}", self.head, self.tail);
-        let mut node = &self.nodes[self.head];
-        let mut numbers: Vec<i64> = Vec::with_capacity(self.nodes.len());
-        loop {
-            numbers.push(node.value);
-            match node.next {
-                Some(next) => {
-                    node = &self.nodes[next];
-                }
-                None => break,
-            }
-        }
-        println!("{numbers:?}");
+    fn swap(&mut self, i: usize, j: usize) {
+        self.nodes.swap(i, j);
+        self.indices.swap(self.nodes[i].original_index, self.nodes[j].original_index);
     }
 }
 
-fn solve(input: &str) -> i64 {
-    let numbers = parse_input(input);
-
-    let mut list = NumberList::from_vec(numbers);
-    for i in 0..list.nodes.len() {
-        list.move_num(i);
-    }
-
-    list.get(1000) + list.get(2000) + list.get(3000)
-}
-
-fn solve_part_2(input: &str) -> i64 {
+fn solve(input: &str, multiplier: i64, iterations: usize) -> i64 {
     let numbers: Vec<_> = parse_input(input).into_iter().map(|n| {
-        n * 811589153
+        n * multiplier
     }).collect();
     let mut list = NumberList::from_vec(numbers);
 
-    for _ in 0..10 {
+    for _ in 0..iterations {
         for i in 0..list.nodes.len() {
             list.move_num(i);
         }
     }
 
     list.get(1000) + list.get(2000) + list.get(3000)
+}
+
+fn solve_part_1(input: &str) -> i64 {
+    solve(input, 1, 1)
+}
+
+fn solve_part_2(input: &str) -> i64 {
+    solve(input, 811589153, 10)
 }
 
 fn parse_input(input: &str) -> Vec<i64> {
@@ -199,7 +86,7 @@ fn parse_input(input: &str) -> Vec<i64> {
 fn main() {
     let input = advent_of_code_2022::read_input().expect("unable to read input file");
 
-    let solution1 = solve(&input);
+    let solution1 = solve_part_1(&input);
     println!("{solution1}");
 
     let solution2 = solve_part_2(&input);
@@ -214,7 +101,7 @@ mod tests {
 
     #[test]
     fn test_sample_input_part_1() {
-        assert_eq!(3, solve(SAMPLE_INPUT));
+        assert_eq!(3, solve_part_1(SAMPLE_INPUT));
     }
 
     #[test]
